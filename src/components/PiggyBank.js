da7fb0043e90aeb94e66a75640fbe22a577bfc8e
erichardson <egrichardson7@gmail.com>
2023-10-28 20:25:31 -0600

    animation sort of works

--- src/components/PiggyBank.js
+++ src/components/PiggyBank.js
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
-import Coin from "./Coin";
 
 function PiggyBank() {
   const [balance, setBalance] = useState(0);
   const [isDepositing, setIsDepositing] = useState(false);
   const [isWithdrawing, setIsWithdrawing] = useState(false);
+  const [isCoinInside, setIsCoinInside] = useState(true);
+  const savingsGoal = 100;
+  const percentageToGoal = (balance / savingsGoal) * 100;
 
   // deposit $
   const deposit = (amount) => {
     setIsDepositing(true);
+    setIsCoinInside(true); // Coin is inside when depositing
     setTimeout(() => {
       setBalance(balance + amount);
       setIsDepositing(false);
-    }, 500); // Adjust the timing to match your animation duration
+    }, 500);
   };
 
-  // withdraw $
   const withdraw = (amount) => {
     if (balance >= amount) {
       setIsWithdrawing(true);
+      setIsCoinInside(false); // Coin is outside when withdrawing
       setTimeout(() => {
         setBalance(balance - amount);
         setIsWithdrawing(false);
@@ -29,17 +32,51 @@ function PiggyBank() {
   };
 
   // display piggy bank image and deposit/withdraw buttons
+  // show current balance, savings goal, and percentage to reach savings goal
   return (
     <div className="body">
       <h1>Piggy Pay</h1>
-      <img src="pig.png" alt="Pig" class="center"></img>
-      <div className="coin-container">
-        <Coin isDepositing={isDepositing} />
-        <Coin isDepositing={isWithdrawing} />
+      <div>
+        <p>Balance: ${balance.toFixed(2)}</p>
+        <p>Savings Goal: ${savingsGoal.toFixed(2)}</p>
+        <p>Percentage to Savings Goal: {percentageToGoal.toFixed(0)}%</p>
       </div>
-      <p>Balance: ${balance.toFixed(2)}</p>
-      <button onClick={() => deposit(10)}>Deposit $10</button>
-      <button onClick={() => withdraw(10)}>Withdraw $10</button>
+      <div className="coin-container">
+        {isCoinInside ? (
+          <div className={`coin deposit`}>${isDepositing}</div>
+        ) : (
+          <div className={`coin withdraw`}>${isWithdrawing}</div>
+        )}
+      </div>
+      <div className="piggy-container">
+        <img src="pig.png" alt="Pig" className="piggy"></img>
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder="Enter deposit amount"
+          value={isDepositing}
+          onChange={(e) => setIsDepositing(e.target.value)}
+        />
+        <br></br>
+        <button onClick={() => deposit(parseFloat(isDepositing))}>
+          Deposit
+        </button>
+      </div>
+      <br></br>
+
+      <div>
+        <input
+          type="number"
+          placeholder="Enter withdrawal amount"
+          value={isWithdrawing}
+          onChange={(e) => setIsWithdrawing(e.target.value)}
+        />
+        <br></br>
+        <button onClick={() => withdraw(parseFloat(isWithdrawing))}>
+          Withdraw
+        </button>
+      </div>
     </div>
   );
 }
